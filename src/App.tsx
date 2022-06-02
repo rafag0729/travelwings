5424d5c621464c9793563591ee7523189ad03626
Rafael Gallego <rafaga_729@hotmail.com>
2022-06-02 08:54:31 -0500

    custom theme added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppRouter } from './router/AppRouter';
+import { theme } from './theme/theme';
 
 
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={ theme }>
       <AppRouter />
     </ChakraProvider>
   );
---
+++ src/theme/theme.tsx
@@ -0,0 +1,14 @@
+import { extendTheme } from '@chakra-ui/react';
+
+
+export const theme = extendTheme({
+    colors: {
+        brand: {
+            red: '#fd0100ff',
+            purple: '#84495aff',
+            blue: '#1291b1ff',
+            green: '#81d060ff',
+            yellow: '#f5f616ff'       
+        }
+    }
+})
