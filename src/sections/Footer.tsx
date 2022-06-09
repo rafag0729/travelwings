b5b43cc5968aeddceba9df6032855ff48d565e5d
Rafael Gallego <rafaga_729@hotmail.com>
2022-06-09 16:00:15 -0500

    Structure for footer added

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,10 +1,27 @@
-import { Container } from "@chakra-ui/react"
+import { Container, Flex, Heading, Text } from "@chakra-ui/react"
 
 
 export const Footer = () => {
   return (
-    <Container maxW="container.lg" bg="green.100"> 
-        Footer
+    <Container maxW="full" bgGradient="linear(to-r, brand.red, brand.blue)"> 
+      <Container p="6" maxW="container.lg">
+        <Heading color="white" fontSize="2xl" mb="6">
+          Contactanos
+        </Heading>
+
+        <Flex mb="6" flexDirection="column">
+          <Text fontSize="sm" color="white">Pereira, Risaralda, Colombia</Text>
+          <Text fontSize="sm" color="white">304 524 5250</Text>
+          <Text fontSize="sm" color="white">rafaga_729@hotmail.com</Text>
+        </Flex> 
+
+        <Flex direction="row" justifyContent="space-between">
+          <Text fontSize="xs" color="white">Política de privacidad</Text>
+          <Text fontSize="xs" color="white">Terminos de servicio</Text>
+          <Text fontSize="xs" color="white">Política de cookies</Text>
+          <Text fontSize="xs" color="white">© 2022. Travel Wings S.A.S. Todos los derechos reservados</Text>
+        </Flex>
+      </Container>
     </Container>
   )
 }
