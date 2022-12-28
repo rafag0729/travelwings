f579d6a892a25a6cb6f924d93f4da5a0574a4d91
Rafael Gallego <prv_rafael.gallego@dale.com.co>
2022-12-27 23:23:09 -0500

    Firebase store installation added

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDrazuQq4eR3-HMD6WHJDlLmmhqUoqV9bY",
+    authDomain: "travel-wings-10137.firebaseapp.com",
+    projectId: "travel-wings-10137",
+    storageBucket: "travel-wings-10137.appspot.com",
+    messagingSenderId: "783160790627",
+    appId: "1:783160790627:web:d737c59b5e84940de93256",
+    measurementId: "G-QYJGBXRY75"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
