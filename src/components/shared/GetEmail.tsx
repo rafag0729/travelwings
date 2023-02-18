ffa28246297c296b366a88a3f1d37b5eeb3fb62e
Rafael Gallego <prv_rafael.gallego@dale.com.co>
2023-02-17 22:01:27 -0500

    Get email format adjusted

--- src/components/shared/GetEmail.tsx
+++ src/components/shared/GetEmail.tsx
@@ -10,69 +10,69 @@ interface Props {
 
 export const GetEmail = ({title}: Props) => {
 
-    const [flag, setFlag] = useBoolean();
+    const [ flag, setFlag ] = useBoolean();
     const { email, handleFormChange, error } = useForm({email: ''});
 
     return (
         <Flex
-            flexDir="column"
-            flexGrow={2}
-            mx="2"
+          flexDir="column"
+          flexGrow={2}
+          mx="2"
         >
             {   title && (
-                <HStack mb="3">
-                    <Checkbox
-                        fontSize="sm"
-                        colorScheme="white"
-                        size="md"
-                        isChecked={flag}
-                        onChange={setFlag.toggle}
-                    />
-                    <Text
-                        fontSize="sm"
-                        color="white"
-                        >{title}</Text>
-                </HStack>
+              <HStack mb="3">
+                <Checkbox
+                  fontSize="sm"
+                  colorScheme="white"
+                  size="md"
+                  isChecked={flag}
+                  onChange={setFlag.toggle}
+                />
+                <Text
+                  fontSize="sm"
+                  color="white"
+                  >{title}</Text>
+              </HStack>
             )}
             <Fade
-                in={flag}
+              in={flag}
             >
                 <Flex
-                    flexDirection="row"
-                    mb="5"
-                    bgColor="white"
-                    borderRadius="base"
-                    p="1"
-                    position="relative"
+                  flexDirection="row"
+                  mb="5"
+                  bgColor="white"
+                  borderRadius="base"
+                  p="1"
+                  position="relative"
                 >
                     <FormControl isInvalid={error.error}>
                         <Input 
-                            size="sm"
-                            placeholder='correo@correo.com'
-                            type="email"
-                            variant="flushed"
-                            isInvalid={error.error}
-                            mx="1"
-                            name="email"
-                            value={email}
-                            onChange={(e) => handleFormChange('email', e.target.value)}
+                          size="sm"
+                          placeholder='correo@correo.com'
+                          type="email"
+                          variant="flushed"
+                          isInvalid={error.error}
+                          mx="1"
+                          name="email"
+                          value={email}
+                          onChange={(e) => handleFormChange('email', e.target.value)}
                         />
                         { error.error && (
-                            <FormErrorMessage
-                                position="absolute"
-                                bottom="-6"
-                                bgColor="red"
-                                borderRadius="sm"
-                                color="white"
-                                px="1"
-                            >
-                                {error.msg}</FormErrorMessage>) } 
+                          <FormErrorMessage
+                            position="absolute"
+                            bottom="-6"
+                            bgColor="red"
+                            borderRadius="sm"
+                            color="white"
+                            px="1"
+                          >
+                            {error.msg}</FormErrorMessage>) } 
                     </FormControl>
                     <Button
-                        size="sm"
-                        disabled={error.error}
+                      size="sm"
+                      disabled={error.error}
                     >
-                        Enviar
+                      Enviar
                     </Button>
                 </Flex> 
             </Fade>
