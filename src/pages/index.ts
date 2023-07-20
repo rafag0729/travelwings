0bb3a7ec41db8367b6006818727048fe375a7c3b
Rafael Gallego <prv_rafael.gallego@dale.com.co>
2023-07-19 21:40:37 -0500

    Pages exporter file adjusted

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,5 @@
 export * from 'pages/HomeScreen';
 export * from 'pages/ErrorScreen';
-export * from 'pages/AdminTW';
-export * from 'pages/Login';
+export * from 'pages/AdminScreen';
+export * from 'pages/LoginScreen';
 export * from 'pages/OfferDetails';
