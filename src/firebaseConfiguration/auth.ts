577c49f5111813fab84a49a33d4f5b9ca955a21c
Rafael Gallego <prv_rafael.gallego@dale.com.co>
2023-07-22 15:11:27 -0500

    Auth Fb configuration added

--- src/firebaseConfiguration/auth.ts
+++ src/firebaseConfiguration/auth.ts
@@ -1,7 +1,11 @@
-import { getAuth } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import firebaseApp from './config'
 
-
 const auth = getAuth(firebaseApp);
+auth.useDeviceLanguage();
+
+// Google configuration
+export const provider = new GoogleAuthProvider();
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
 
 export default auth;
