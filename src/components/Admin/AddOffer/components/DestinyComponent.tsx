3b54608caccb4b8fe267399626e6e53f9ed1c192
Rafael Gallego <rafaga_729@hotmail.com>
2023-09-11 21:10:18 -0500

    Destiny component added

--- src/components/Admin/AddOffer/components/DestinyComponent.tsx
+++ src/components/Admin/AddOffer/components/DestinyComponent.tsx
@@ -1,18 +1,44 @@
 import { Box } from '@chakra-ui/react'
+import { useState } from 'react'
 import { GiPalmTree } from 'react-icons/gi'
 import { AddInput, OfferHeading, TagItem } from '../shared'
 
+interface DestinyComponentProps {
+  getDestinyList: (list: string[]) => void;
+}
 
-export const DestinyComponent = () => {
+export const DestinyComponent = ({getDestinyList}: DestinyComponentProps) => {
+
+  const [tagsList, setTagsList] = useState<string[]>([]);
+  
+  const updateList = (result: string[]) => {
+    setTagsList(result);
+    getDestinyList(result)
+  }
+
   return (
     <Box mb="6" minW="">
       <OfferHeading title="DESTINO" />
       <AddInput 
         placeholder='Añadir destino'
         Icon={GiPalmTree}
+        action={(item: string) => {
+          const filter = tagsList.filter(t => t.toLowerCase().trim() === item.toLowerCase().trim())
+          if(filter.length >= 1) return;
+          const result = [...tagsList, item];
+          updateList(result);
+        }}
       />
-      <TagItem item="Santa Marta"/>
-      <TagItem item="Guajira"/>
+      { tagsList.map((t, i) => (
+        <TagItem 
+          key={i.toString()}
+          item={t}
+          deleteAction={(item: string) => {
+            const filter = tagsList.filter((t) => t.toLowerCase().trim() !== item.toLowerCase().trim() )
+            updateList(filter);
+          }}
+        />
+      ))}
     </Box>
   )
 }
