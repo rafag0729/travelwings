19451406d4539ef7c14928ef418ef40559e72c69
Rafael Gallego <rafaga_729@hotmail.com>
2023-09-11 21:35:12 -0500

    Add ioffer component advance

--- src/components/Admin/AddOffer/AddOffer.tsx
+++ src/components/Admin/AddOffer/AddOffer.tsx
@@ -1,33 +1,41 @@
-import { Box, Container, Flex } from '@chakra-ui/react'
-import { AccommodationComponent, AdditionalsComponent, DestinyComponent, ExclusionsComponent, FeedingComponent, TicketsComponent, TranslationComponent } from './components'
+import { Container, Image } from '@chakra-ui/react'
+import { useState } from 'react'
+import { AccommodationComponent, AdditionalsComponent, DestinyComponent, ExclusionsComponent, FeedingComponent, PreviewOffer, PricingComponent, TicketsComponent, TranslationComponent } from './components'
+import { TicketProps } from './components/interfaces';
 
 
 export const AddOffer = () => {
 
+  const [destinyList, setDestinyList] = useState<string[]>([]);
+  const [ticketDetails, setticketDetails] = useState<TicketProps[]>([]);
+  
   return (
     <Container 
       mt="14"
       minW="full">
-      <Flex>
-        <Box>
-          <DestinyComponent />
-          <TicketsComponent />
-          <TranslationComponent />
-          <AccommodationComponent />
-          <FeedingComponent />
-          <AdditionalsComponent />
-          <ExclusionsComponent />
-        </Box>
-
-        {/* <Image
+        <PreviewOffer 
+          destiny={destinyList}
+          ticket={ticketDetails}
+        />
+        <DestinyComponent 
+          getDestinyList={(list) => setDestinyList(list)}
+        />
+        <TicketsComponent 
+          getTicketDetails={(ticket) => setticketDetails(ticket)}
+        />
+        <TranslationComponent />
+        <AccommodationComponent />  
+        <FeedingComponent />
+        <AdditionalsComponent />
+        <ExclusionsComponent />
+        <PricingComponent />
+        <Image
           borderStyle="dashed"
           borderColor="green.300"
           borderWidth="medium"
           boxSize="md"
           borderRadius="lg"
-          ml="8"
-        /> */}
-      </Flex>
+          ml="8"/>
     </Container>
   )
 }
