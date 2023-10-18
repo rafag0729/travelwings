2ccc1c1dd1c41a0f9c497e710308b2b354c20dd3
Rafael Gallego <rafaga_729@hotmail.com>
2023-10-18 17:18:24 -0500

    AddInput logic updated

--- src/components/Admin/AddOffer/shared/AddInput.tsx
+++ src/components/Admin/AddOffer/shared/AddInput.tsx
@@ -1,13 +1,18 @@
 import { IconButton, Input, InputGroup, InputLeftElement, InputRightElement } from '@chakra-ui/react'
+import { useForm } from 'hooks';
 import { IconType } from 'react-icons';
 import { FaPlus } from 'react-icons/fa'
 
 interface AddInputProps {
   placeholder: string;
-  Icon: IconType
+  Icon: IconType;
+  action: (value: string) => void;
 }
 
-export const AddInput = ({ placeholder, Icon }: AddInputProps) => {
+export const AddInput = ({ placeholder, Icon, action }: AddInputProps) => {
+
+  const {value, handleFormChange} = useForm({value: ''});
+
   return (
     <InputGroup mb="4">
       <InputLeftElement
@@ -17,12 +22,19 @@ export const AddInput = ({ placeholder, Icon }: AddInputProps) => {
       <Input
         type="text"
         placeholder={placeholder}
+        value={value}
+        onChange={(e) => handleFormChange('value', e.target.value) }
         focusBorderColor="green.500"/>  
       <InputRightElement
         children={<IconButton 
           colorScheme="teal"
           size="sm"
+          disabled={value.length <= 2}
           aria-label="add-offer" 
+          onClick={() => {
+            action(value);
+            handleFormChange('value', '');
+          }}
           icon={<FaPlus />}/>}/>
     </InputGroup>
   )
