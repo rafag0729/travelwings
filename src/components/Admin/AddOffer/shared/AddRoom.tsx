13b6ee037747d63b941491eca9363ff3a57882de
Rafael Gallego <rafaga_729@hotmail.com>
2023-10-18 17:14:09 -0500

    AddRoom component and logic added

--- src/components/Admin/AddOffer/shared/AddRoom.tsx
+++ src/components/Admin/AddOffer/shared/AddRoom.tsx
@@ -1,26 +1,58 @@
-import { Flex, IconButton, Input, InputGroup, InputLeftElement, InputRightElement, NumberInput, NumberInputField, Select } from "@chakra-ui/react"
-import { FaBed, FaPlus } from "react-icons/fa"
+import { Flex, IconButton, InputGroup, InputRightElement, NumberInput, NumberInputField, Select } from "@chakra-ui/react"
+import { useState } from "react"
+import { FaPlus } from "react-icons/fa"
+import { RoomDetailType } from "../components/interfaces";
+import { TagItem } from "./TagItem";
 
+interface AddRoomProps {
+  getRooms: (rooms: RoomDetailType[]) => void;
+}
 
-export const AddRoom = () => {
+export const AddRoom = ({getRooms}: AddRoomProps) => {
+
+  const [roomType, setRoomType] = useState('')
+  const [roomQuantity, setRoomQuantity] = useState('');
+
+  const [roomDetails, setRoomDetails] = useState<RoomDetailType[]>([]);
+
+  const handleAddRoom = (type: string, quantity: string) => {
+    const filter = roomDetails.filter((r) => r.type === type);
+    if (filter.length >= 1) return updateRoomDetails(type, quantity);
+    const rooms = [...roomDetails, {type, quantity}]
+    getRooms(rooms);
+    setRoomDetails(rooms);
+  }
+
+  const updateRoomDetails = (type: string, quantity: string) => {
+    const map = roomDetails.map((r) => {
+      if (r.type === type) {
+        return {
+          type: r.type,
+          quantity: (Number(r.quantity) + Number(quantity)).toString()
+        }
+      }
+      return r;
+    })
+    getRooms(map);
+    setRoomDetails(map);
+  }
+
+  const handleRemoveRoom = (type: string) => {
+    const filter = roomDetails.filter(r => r.type !== type);
+    getRooms(filter);
+    setRoomDetails(filter);
+  }
+
   return (
-    <Flex>
-      <InputGroup mb="4">
-        {/* <InputLeftElement
-          pointerEvents="none"
-          color='gray.300'
-          children={<FaBed />}/> 
-        <NumberInput
-          placeholder='Añadir habitación'
-          focusBorderColor="green.500"/>  
-        <InputRightElement
-          children={<IconButton
-            colorScheme="teal"
-            size="sm"
-            aria-label="add-offer" 
-            icon={<FaPlus />}/>}/> */}
-
-        <Select placeholder='Selecciona el tipo de habitación'>
+    <>
+    <InputGroup 
+      mb="4">
+      <Flex 
+        direction="row">
+        <Select 
+          flexBasis="400"
+          onChange={({target}) => setRoomType(target.value)}
+          placeholder='Selecciona el tipo de habitación'>
           <option value='sencilla'>Sencilla</option>
           <option value='doble'>Doble</option>
           <option value='triple'>Triple</option>
@@ -28,16 +60,32 @@ export const AddRoom = () => {
           <option value='quintuple'>Quintuple</option>
           <option value='sectuple'>Sectuple</option>
         </Select>
-      </InputGroup>
-      <NumberInput defaultValue={1} min={0} max={5}>
+        <NumberInput
+          flexBasis="100"
+          onChange={(e) => setRoomQuantity(e)}
+          defaultValue={0} 
+          min={0} 
+          max={5}>
         <NumberInputField/>
         <InputRightElement
           children={<IconButton
             colorScheme="teal"
             size="sm"
             aria-label="add-offer" 
+            disabled={!roomQuantity || roomQuantity === '0' || !roomType}
+            onClick={() => handleAddRoom(roomType, roomQuantity)}
             icon={<FaPlus />}/>}/>
-      </NumberInput>
+        </NumberInput>
+      </Flex>
+    </InputGroup>
+    <Flex flexGrow={2}>
+      { roomDetails.map((r, i) => (
+        <TagItem
+          key={r.quantity+i}
+          item={`${r.quantity} - ${r.type}`} 
+          deleteAction={() => handleRemoveRoom(r.type)} />
+      ))}
     </Flex>
+  </>
   )
 }
