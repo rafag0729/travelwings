ea9094c3e779e8edd83fe014294c81be0f92b59c
Rafael Gallego <rafaga_729@hotmail.com>
2023-10-18 17:19:52 -0500

    Accomodation component logic updated

--- src/components/Admin/AddOffer/components/AccommodationComponent.tsx
+++ src/components/Admin/AddOffer/components/AccommodationComponent.tsx
@@ -1,29 +1,68 @@
-import { Box, Radio, RadioGroup, Stack } from '@chakra-ui/react'
+import { Box, Button, Radio, RadioGroup, Stack } from '@chakra-ui/react'
 import { useState } from 'react'
 import { OfferHeading } from '../shared'
-import { AirbnbInput } from '../shared/AirbnbInput'
-import { HostelInput } from '../shared/HostelInput'
 import { HotelInput } from '../shared/HotelInput'
+import { AccomodationType, AccomodationTypeAndDetails, HotelDetailsProps, HOTEL_DETAILS } from './interfaces'
 
-export const AccommodationComponent = () => {
+interface AccomodationComponentProps {
+  getAccomodationValues: (value: AccomodationTypeAndDetails) => void;
+}
 
-  const [radioAccommodation, setRadioAccommodation] = useState('1')
+export const AccommodationComponent = ({getAccomodationValues}: AccomodationComponentProps) => {
+
+  const [radioAccommodation, setRadioAccommodation] = useState<AccomodationType>('no-accomodation')
+  const [expandDetails, setExpandDetails] = useState(false);
+  
+  const handleAccomodationChange = (value: AccomodationType) => {
+    setRadioAccommodation(value);
+    getAccomodationValues({
+      type: value,
+      details: HOTEL_DETAILS,
+    });
+  }
+
+  const handleHotelInputDetails = (newHotelDetails: HotelDetailsProps) => {
+    getAccomodationValues({
+      type: radioAccommodation,
+      details: newHotelDetails,
+    });
+  }
 
   return (
     <Box mb="6">
       <OfferHeading title='ALOJAMIENTO' />
+      <Button
+        onClick={() => setExpandDetails((prev) => !prev)}
+        size="xs"
+        variant="outline"
+        colorScheme="teal">
+        { !expandDetails ? 'Expandir' : 'Esconder'} detalles de alojamiento
+      </Button>
       <RadioGroup
+        mt="4"
         colorScheme="teal"
-        onChange={setRadioAccommodation} 
+        onChange={handleAccomodationChange} 
         value={radioAccommodation}>
         <Stack direction='column'>
-          <Radio value='1'>Sin alojamiento</Radio>
-          <Radio value='2'>Hotel</Radio>
-          { radioAccommodation === '2' && <HotelInput />}
-          <Radio value='3'>Hostal</Radio>
-          { radioAccommodation === '3' && <HostelInput />}
-          <Radio value='4'>AirBnB</Radio>
-          { radioAccommodation === '4' && <AirbnbInput />}
+          <Radio value='no-accomodation'>Sin alojamiento</Radio>
+          <Radio value='hotel'>Hotel</Radio>
+          { radioAccommodation === 'hotel' && expandDetails && (
+            <HotelInput 
+              hotelType='hotel'
+              getHotelInputDetails={(details) => handleHotelInputDetails(details)}/>
+          )}
+          <Radio value='hostel'>Hostal</Radio>
+          { radioAccommodation === 'hostel' && expandDetails && (
+            <HotelInput 
+              hotelType='hostel'
+              getHotelInputDetails={(details) => handleHotelInputDetails(details)}/>
+          )}
+          <Radio value='airbnb'>AirBnB</Radio>
+          { radioAccommodation === 'airbnb' && expandDetails && (
+            <HotelInput 
+              hotelType='airbnb'
+              getHotelInputDetails={(details) => handleHotelInputDetails(details)}/>
+          )}
         </Stack>
       </RadioGroup>
     </Box>
