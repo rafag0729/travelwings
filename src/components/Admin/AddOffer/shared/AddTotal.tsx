c955c5d3d207a1d16bbda37a96b87f3070b575ab
Rafael Gallego <rafaga_729@hotmail.com>
2023-10-30 16:03:29 -0500

    Total component adjusted

--- src/components/Admin/AddOffer/shared/AddTotal.tsx
+++ src/components/Admin/AddOffer/shared/AddTotal.tsx
@@ -1,15 +1,25 @@
 import { Box, Flex, Input, InputGroup, InputLeftAddon, Text } from '@chakra-ui/react'
+import { currencyMoneyDots } from 'utils/formats';
 
-export const AddTotal = () => {
+interface AddTotalProps {
+  label: string;
+  costAmount: string;
+}
+
+export const AddTotal = ({label, costAmount}: AddTotalProps) => {
   return (
     <Flex direction="column">
       <Text
         mr="2"
-        fontSize="sm">Total viaje</Text>
+        fontSize="sm">{label}</Text>
       <Box>
         <InputGroup>
           <InputLeftAddon children="$"/>
-          <Input type="number" focusBorderColor="green" />
+          <Input 
+            value={currencyMoneyDots(costAmount)}
+            disabled
+            isReadOnly
+            focusBorderColor="green" />
         </InputGroup>
       </Box>
     </Flex>
