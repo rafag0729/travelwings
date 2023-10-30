7493b75320e0428e17581b509de0445e338d7b1d
Rafael Gallego <rafaga_729@hotmail.com>
2023-10-30 16:03:18 -0500

    Price component adjusted

--- src/components/Admin/AddOffer/shared/AddPrice.tsx
+++ src/components/Admin/AddOffer/shared/AddPrice.tsx
@@ -1,14 +1,25 @@
 import { Flex, Input, InputGroup, InputLeftAddon, Text } from '@chakra-ui/react'
 
-export const AddPrice = () => {
+interface AddPriceProps {
+  getPrice: (price: string) => void;
+  label: string;
+}
+
+export const AddPrice = ({getPrice, label}: AddPriceProps) => {
   return (
     <Flex
       mr="4"
       direction="column">
-      <Text fontSize="sm">Costo por persona</Text>
+      <Text fontSize="sm">{label}</Text>
       <InputGroup>
         <InputLeftAddon children="$"/>
-        <Input type="number" focusBorderColor="green" />
+        <Input 
+          type="number"
+          min={0}
+          onChange={({target}) => {
+            getPrice(target.value)
+          }}
+          focusBorderColor="green" />
       </InputGroup>
     </Flex>
   )
